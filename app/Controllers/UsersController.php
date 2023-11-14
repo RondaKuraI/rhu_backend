1da71eaa6766256effcde06b2504c1d972c3ac54
RondaKuraI <johnronda0527@gmail.com>
2023-11-15 00:12:43 +0800

    Update UsersController.php

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -19,6 +19,24 @@ class UsersController extends BaseController
         $data = [];
         helper(['form']);
 
+        if ($this->request->getMethod() == 'post') {
+            #Validation
+            $rules = [
+                'firstname' => 'required|min_length[3]|max_length[20]',
+                'lastname' => 'required|min_length[3]|max_length[20]',
+                'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]',
+                'password' => 'required|min_length[8]|max_length[255]',
+                'conpassword' => 'matches[password]',
+            ];
+
+            if (! $this->validate($rules)) {
+                $data['validation'] = $this->validator;
+            } else {
+                # code...
+            }
+            
+        }
+
         return view('register');
     }
 }
